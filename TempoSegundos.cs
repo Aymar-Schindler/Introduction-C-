6c651c1fec3766c4110e4fbcfe5899926d83d521
Aymar Schindler <102153365+Aymar-Schindler@users.noreply.github.com>
2022-03-29 12:33:06 -0300

    Update TempoSegundos.cs

--- TempoSegundos.cs
+++ TempoSegundos.cs
@@ -1,6 +1,6 @@
 /******************************************************************************************************
 %  Code: 
-%   PostosDeGasolina.cs
+%   TempoSegundos.cs
 %
 %  Purpose:
 %   Implements sample C# code 
