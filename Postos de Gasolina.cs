f637087bdc6b57dcd1c735b268650f3687db638f
Aymar Schindler <102153365+Aymar-Schindler@users.noreply.github.com>
2022-03-29 11:38:20 -0300

    Update Postos de Gasolina.cs

--- Postos de Gasolina.cs
+++ Postos de Gasolina.cs
@@ -1,46 +1,49 @@
-/*%****************************************************************************80
+/******************************************************************************************************
 %  Code: 
-%   Gasolina.cs
+%   PostosDeGasolina.cs
 %
 %  Purpose:
-%   Implements sample c# code 
-%   Exercise #1 - 'Linguagem de Programação: Entradas e Saídas'
+%   Implements sample C# code 
+%   Exercise #1 - Introdução a Linguagem de Programação: Entradas e Saídas
 %   'Escreva um programa capaz de calcular o preço médio da gasolina considerando três postos de 
 %    combustível de Salvador. Solicitando ao usuário que informe o nome e o valor da gasolina em cada
 %    um dos três postos.'
 %
 %  Modified:
-%   Mar 28 2019 10:57 
+%   Mar 29 2022 09:00 
 %
 %  Author:
 %    Murilo Boratto <muriloboratto 'at' uneb.br>
 %    Leandro Correia  <leandro.correia 'at' gmail.com>
+%    Aymar Schindler  <aymarschindler 'at' hotmail.com>
 %
 %  How to Compile:
 %   mcs code.cs -o object 
 %
 %  How to Execute: 
 %   mono object
+%   vs code
 %
-%****************************************************************************80*/
+%*****************************************************************************************/
 
 using System;
 
-namespace myApp
+namespace IntroCS
 { 
-  class Gasolina
+  class ValorGasolina
   {
     static void Main()
     {
         float valorPosto1, valorPosto2, valorPosto3, mediaReaisPrecosGasolinas;
 
-        Console.WriteLine ("Entre com o valor da Gasolina do Posto 1:");
+        Console.WriteLine ("Bem vindo ao programa de média de gasolinas de 03 postos!");  
+        Console.WriteLine ("Insira o valor da Gasolina do Posto 1:");
         valorPosto1 = float.Parse(Console.ReadLine());  
 
-        Console.WriteLine ("Entre com o valor da Gasolina do Posto 2:");
+        Console.WriteLine ("Insira o valor da Gasolina do Posto 2:");
         valorPosto2 = float.Parse(Console.ReadLine()); 
 
-        Console.WriteLine ("Entre com o valor da Gasolina do Posto 3:");
+        Console.WriteLine ("Insira o valor da Gasolina do Posto 3:");
         valorPosto3 = float.Parse(Console.ReadLine()); 
 
         mediaReaisPrecosGasolinas = (valorPosto1 + valorPosto2 + valorPosto3)/3;
