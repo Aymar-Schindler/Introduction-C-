405cef255c369ba2f6486e425fb55b3509a8978d
Aymar Schindler <102153365+Aymar-Schindler@users.noreply.github.com>
2022-04-27 15:40:53 -0300

    Update Loteria.cs

--- MatrizesVetores/Loteria.cs
+++ MatrizesVetores/Loteria.cs
@@ -32,46 +32,92 @@ using System;
 
 namespace IntroCS
 {
-    public class Loteria
+  public class Loteria
   {  
     static void Main()
     {
-      int[,] matrix = new int[4, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-      int linha, coluna, DIAGPRINC=0, SUB=0, SUPER=0;
-
+      //Declação da Matriz Resultados
+      int[,] ResultadosOficiais = new int[13, 3];
+      //Declação da Matriz Apostas
+      int[,] ApostasRealizadas = new int[13, 3];
+      //Variáveis Auxiliares
+      int linha, coluna, pontuacao=0, aposta;
+      //Variável auxiliar randômica
+      var random = new Random();
+  
       Console.Write("\n"); 
 
-      /* Print matrix */
-      for(linha = 0; linha < 4;linha++){
-        for(coluna = 0; coluna < 4; coluna++){     
-          Console.Write("{0}\t", matrix[linha,coluna]);   
+      //Entrada de Dados com insercao na matriz das apostas
+      for(linha = 0; linha < 13;linha++){
+      Console.Write ("Para o jogo de número {0} digite '0' para vitória do primeiro time, '1' para empate ou '2' para vitória do segundo time: ", (linha + 1));
+			aposta = int.Parse (Console.ReadLine ());
+        switch (aposta) {
+          //lançamento da aposta de vitória do primeiro time
+          case 0:
+            ApostasRealizadas[linha,0] = 1;
+            ApostasRealizadas[linha,1] = 0;
+            ApostasRealizadas[linha,1] = 0;
+            break;
+          //lançamento da aposta do empate  
+          case 1:
+            ApostasRealizadas[linha,0] = 0;
+            ApostasRealizadas[linha,1] = 1;
+            ApostasRealizadas[linha,1] = 0;
+            break;
+          //lançamento da aposta de vitória do segundo time
+          case 2:
+            ApostasRealizadas[linha,0] = 0;
+            ApostasRealizadas[linha,1] = 0;
+            ApostasRealizadas[linha,1] = 1;
+            break;
+          //inserção de valor inválido
+          default:
+            Console.WriteLine ("Número inválido para inserir sua aposta!");
+            break;
         }
       Console.Write("\n"); 
       }
 
-      /* Calculate add matrix */
-      for(linha = 0; linha < 4;linha++){
-        for(coluna = 0; coluna < 4; coluna++){ 
-          
-          if (linha == coluna)
-               DIAGPRINC = DIAGPRINC + matrix[linha, coluna];
-  
-          if (linha == coluna - 1)
-              SUB = SUB + matrix[linha, coluna];
-  
-          if (linha == coluna + 1)
-              SUPER = SUPER + matrix[linha, coluna];     
-    
-        }  
-      }    
-    
-      Console.Write("\n"); 
+      //Impressão de Apostas Realizadas
+      Console.WriteLine ("Segue abaixo o seu cartão de apostas: \n");
+      for(linha = 0; linha < 13;linha++){
+        for(coluna = 0; coluna < 3; coluna++){     
+          Console.Write("{0}\t", ApostasRealizadas[linha,coluna]);   
+        }
+        Console.Write("\n"); 
+      }
+      Console.Write("\n");
+      
+      //Loop para randomizar os resultados
+      for(linha = 0; linha < 13;linha++){
+          coluna = random.Next()%3;   
+          ResultadosOficiais[linha,coluna] = 1;
+      }
+      
+      //Impressão dos Resultados Oficiais
+      Console.WriteLine ("O resultado saiu! Veja o cartão de Resultados Oficiais abaixo: \n");
+      for(linha = 0; linha < 13;linha++){
+        for(coluna = 0; coluna < 3; coluna++){     
+          Console.Write("{0}\t", ResultadosOficiais[linha,coluna]);   
+        }
+        Console.Write("\n"); 
+      }
+      Console.Write("\n");
+      
+      //Comparação das Duas Matrizes
+      for(linha = 0; linha < 13;linha++){
+         for(coluna = 0; coluna < 3; coluna++){
+            if (ResultadosOficiais[linha,coluna] == 1) {
+               if (ResultadosOficiais[linha,coluna] == ApostasRealizadas[linha,coluna]) {
+                  pontuacao++;
+               }
+            }
+         }
+      }
+      Console.Write("\n");
 
-      /* Print Results */
-      Console.WriteLine ("A soma da DIAGONAL PRINCIPAL Ã© {0}", DIAGPRINC);   
-      Console.WriteLine ("A soma da SUBDIAGONAL Ã© {0}", SUB);   
-      Console.WriteLine ("A soma da SUPERDIAGONAL Ã© {0}", SUPER);   
-
-    }/*main*/
+      //Impressão dos pontos
+      Console.WriteLine("PARABÉNS! Apostador, você conseguiu alcançar nas apostas acima {0} pontos \n!", pontuacao);
+    }
   }
 }
