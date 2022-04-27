01c43c3a92dd110ba5184666d2b8e55a00382030
Aymar Schindler <102153365+Aymar-Schindler@users.noreply.github.com>
2022-04-27 13:38:30 -0300

    Update Loteria

--- MatrizesVetores/Loteria.cs
+++ MatrizesVetores/Loteria.cs
@@ -32,20 +32,46 @@ using System;
 
 namespace IntroCS
 {
-    public class MenorValor
-	{
-		public static void Main ()
-		{
-    // Declaração de Variáveis
-		int cont, maior = 0;
-    do {
-        cout << "digite um valor: ";
-        cin >> cont;
-        //Se o valor entrado for maior que o ultimo passa ele a ser o maior
-        if(cont > maior)maior = cont;
-    }while (cont != -1);
-    cout << "o maior numero e: " << maior;
-    return 0;
-    }
-  }    
+    public class Loteria
+  {  
+    static void Main()
+    {
+      int[,] matrix = new int[4, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+      int linha, coluna, DIAGPRINC=0, SUB=0, SUPER=0;
+
+      Console.Write("\n"); 
+
+      /* Print matrix */
+      for(linha = 0; linha < 4;linha++){
+        for(coluna = 0; coluna < 4; coluna++){     
+          Console.Write("{0}\t", matrix[linha,coluna]);   
+        }
+      Console.Write("\n"); 
+      }
+
+      /* Calculate add matrix */
+      for(linha = 0; linha < 4;linha++){
+        for(coluna = 0; coluna < 4; coluna++){ 
+          
+          if (linha == coluna)
+               DIAGPRINC = DIAGPRINC + matrix[linha, coluna];
+  
+          if (linha == coluna - 1)
+              SUB = SUB + matrix[linha, coluna];
+  
+          if (linha == coluna + 1)
+              SUPER = SUPER + matrix[linha, coluna];     
+    
+        }  
+      }    
+    
+      Console.Write("\n"); 
+
+      /* Print Results */
+      Console.WriteLine ("A soma da DIAGONAL PRINCIPAL Ã© {0}", DIAGPRINC);   
+      Console.WriteLine ("A soma da SUBDIAGONAL Ã© {0}", SUB);   
+      Console.WriteLine ("A soma da SUPERDIAGONAL Ã© {0}", SUPER);   
+
+    }/*main*/
+  }
 }
